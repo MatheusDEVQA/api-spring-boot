e7bff071c1fc9b77258a2463e698039979c798f7
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2022-10-28 21:30:38 -0300

    Controller Usuario

---
+++ src/main/java/com/springboot/springboot/controller/UsusarioController.java
@@ -0,0 +1,42 @@
+package com.springboot.springboot.controller;
+
+import com.springboot.springboot.model.Usuario;
+import com.springboot.springboot.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsusarioController {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @PostMapping
+    public  void postUsuario(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getByIdUsuario(@PathVariable("id") Integer id){
+        return repository.findById(id);
+    }
+
+    @PutMapping
+    public void updateUsuario(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
+
+    @DeleteMapping
+    public void deleteUsuario(@PathVariable("id") Integer id){
+        repository.delete(id);
+    }
+
+}
